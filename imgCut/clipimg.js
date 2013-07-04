7f362fc7d7840efdbed0bdc9bc73c6e7f2d6c917
wisteriaflash <wisterialflash@163.com>
2013-07-04 11:00:04 +0800

    插件imgclip 完成80%，待完成缩放功能

--- imgCut/clipimg.js
+++ imgCut/clipimg.js
@@ -5,67 +5,160 @@
  */
 (function($){
 
-var conf = {x:150, y:85};
-var container = {w: 202, h:202};
+// var conf = {x:150, y:85};
+// var container = {w: 202, h:202};
 
 
-var dragImgaa = {
-    box: {sw:202, sh:202},
-    dragItem: null,
-    init: function(){
-    var me = this;
-    dragItem = $("#draggable").draggable({
-        containment: me.setImgLimit(500,370),
-        scroll: false,
-        start: function() {
+// var dragImgaa = {
+//     box: {sw:202, sh:202},
+//     dragItem: null,
+//     init: function(){
+//     var me = this;
+//     dragItem = $("#draggable").draggable({
+//         containment: me.setImgLimit(500,370),
+//         scroll: false,
+//         start: function() {
           
+//         },
+//         drag: function(event, ui) {
+//         var top = ui.position.top,
+//             left = ui.position.left;
+//         me.setImgMask(left, top);
+//         },
+//         stop: function(event, ui) {
+
+//         }
+//         });
+//     },
+//     setImgLimit: function(sw,sh){
+//     var imgOffset = $('.img-con').offset();
+//     var offsetLeft = imgOffset.left + parseInt($('.img-con').css('border-left-width'));
+//     var offsetTop = imgOffset.top + parseInt($('.img-con').css('border-top-width'));
+//     var swfix = conf.x+1, shfix = conf.y+1;
+//     // console.log(imgOffset)
+//     //
+//     var x2 = offsetLeft + swfix;
+//     var y2 = offsetTop + shfix;
+//     var x1 = x2 - (sw - container.w);
+//     var y1 = y2 - (sh - container.h);
+//     // var arr = [163,-66,461,101];
+//     var arr = [x1,y1,x2,y2];
+//     return arr;
+//     },
+//     setImgMask: function(left,top){
+//         left += conf.x;
+//         top += conf.y;
+//         // debugger;
+//         $('.img').css({
+//           'left': left,
+//           'top': top
+//         });
+//     }
+// }
+// dragImgaa.init()
+
+
+
+
+
+
+
+//plugin start============
+//private fun
+var _getSettings = function(){
+    var $this = $(this);
+    return $this.data('clipimg');
+}
+var _setSettings = function(options){
+    var $this = $(this);
+    $this.data('clipimg', options);
+}
+var _renderHtml = function(){
+    var $this = $(this);
+    var settings = _getSettings.call($this);
+    var image = settings.image;
+    var clipbox = settings.clipbox;
+    //imgMask
+    var str = '<div class="clipimg-mask">'+
+                '<img src="'+image.source+'" width="'+image.width+'" height="'+image.height+'" />'+
+            '</div>'+
+            '<div class="clipimg-clipbox">'+
+                '<img src="'+image.source+'" width="'+image.width+'" height="'+image.height+'" />'+
+            '</div>';
+    //main
+    $this.css({
+        width: image.width,
+        height: image.height
+    });
+
+    $this.addClass('clipimg');
+    $this.html(str);
+    //init css set
+    var clipboxBorder = $this.find('.clipimg-clipbox').css('border-width');
+    clipboxBorder = parseInt(clipboxBorder);
+    $this.find('.clipimg-clipbox').css({
+        width: clipbox.width-2*clipboxBorder,
+        height: clipbox.height-2*clipboxBorder,
+        left: clipbox.x,
+        top: clipbox.y
+    }).find('img').css({
+        left: -clipbox.x,
+        top: -clipbox.y
+    })
+};
+var _bindHandler = function(){
+    var $this = $(this);
+    var item = $this.find('.clipimg-clipbox img');
+    item.draggable({
+        containment: _setImgLimit.call($this),
+        // scroll: false,
+        // cursor: "crosshair",
+        start: function() {
+
         },
         drag: function(event, ui) {
-        var top = ui.position.top,
-            left = ui.position.left;
-        me.setImgMask(left, top);
+            var settings = _getSettings.call($this);
+            var clipbox = settings.clipbox;
+            var top = ui.position.top,
+                left = ui.position.left;
+            left += clipbox.x;
+            top += clipbox.y;
+            $this.find('.clipimg-mask').css({
+              'left': left,
+              'top': top
+            });
         },
         stop: function(event, ui) {
-
+            
         }
-        });
-    },
-    setImgLimit: function(sw,sh){
-    var imgOffset = $('.img-con').offset();
-    var offsetLeft = imgOffset.left + parseInt($('.img-con').css('border-left-width'));
-    var offsetTop = imgOffset.top + parseInt($('.img-con').css('border-top-width'));
-    var swfix = conf.x+1, shfix = conf.y+1;
+    });
+    //refresh opthion
+    item.mousedown(function(){
+        item.draggable("option", "containment", _setImgLimit.call($this));
+    });
+};
+var _setImgLimit = function(){
+    var $this = $(this);
+    var settings = _getSettings.call($this);
+    var image = settings.image;
+    var clipbox = settings.clipbox;
+    //
+    var imgOffset = $this.offset();
+    var offsetLeft = imgOffset.left + parseInt($this.css('border-left-width'));
+    var offsetTop = imgOffset.top + parseInt($this.css('border-top-width'));
+    var swfix = clipbox.x+1, shfix = clipbox.y+1;
     //
     var x2 = offsetLeft + swfix;
     var y2 = offsetTop + shfix;
-    var x1 = x2 - (sw - container.w);
-    var y1 = y2 - (sh - container.h);
-    // var arr = [163,-66,461,101];
+    var x1 = x2 - (image.width - clipbox.width);
+    var y1 = y2 - (image.height - clipbox.height);
     var arr = [x1,y1,x2,y2];
     return arr;
-    },
-    setImgMask: function(left,top){
-    left += conf.x;
-    top += conf.y;
-    // debugger;
-    $('.img').css({
-      'left': left,
-      'top': top
-    });
-    }
 }
 
 
 
-
-dragImgaa.init()
-
-//plugin start============
-
-var privateFunction = function() {
-    // 代码在这里运行
-}
-
+//public fun
 var methods = {
     init: function(options) {
         return this.each(function() {
@@ -73,21 +166,14 @@ var methods = {
             var settings = $this.data('clipimg');
 
             if(typeof(settings) == 'undefined') {
-
-                var defaults = {
-                    propertyName: 'value',
-                    onSomeEvent: function() {}
-                }
-
-                settings = $.extend({}, defaults, options);
-
-                $this.data('clipimg', settings);
+                settings = $.extend(true, $.fn.clipimg.defaults, options);
             } else {
-                settings = $.extend({}, settings, options);
+                settings = $.extend(true, settings, options);
             }
-
-            // 代码在这里运行
-            console.log('init');
+            //
+            _setSettings.call($this, settings);
+            _renderHtml.call($this);
+            _bindHandler.call($this);
         });
     },
     destroy: function(options) {
@@ -121,5 +207,21 @@ $.fn.clipimg = function() {
 
 }
 
+// defaults
+$.fn.clipimg.defaults = {
+    width: 500,
+    height: 400,
+    clipbox: {
+        x: 0,
+        y: 0,
+        width: 200,
+        height: 200
+    },
+    image: {
+        source: '',
+        width: 0,
+        height: 0
+    }
+}
 
 })(jQuery);
