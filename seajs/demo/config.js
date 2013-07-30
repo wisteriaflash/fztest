875ccf36404523bd318645dab58dcc94852fe7ab
wisteriaflash <wisterialflash@163.com>
2013-07-30 09:02:22 +0800

    init demo

---
+++ seajs/demo/config.js
@@ -0,0 +1,7 @@
+//config
+seajs.config({
+    base: "seajs/demo/lib/",
+    alias: {
+        'jquery': 'jquery/jquery/1.10.1/jquery.js'
+    }
+})
---
+++ seajs/demo/main1.js
@@ -0,0 +1,7 @@
+define(function(require, exports, module){
+    var $ = require('jquery');
+    // exports
+    var str = $('#testp').text();
+    console.log(str);
+    // debugger;
+});
