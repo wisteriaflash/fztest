fc2aa32f8c28c108b97864c9a7e7b263172a80e3
wisteriaflash <wisterialflash@163.com>
2013-08-28 14:13:50 +0800

    add test

---
+++ nodejs/nodeblog/app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , markdown = require('markdown-js');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+//test
+app.get('/markdown', function(req,res){
+    var html = markdown.makeHtml('[Java Eye](http://www.iteye.com/ \'Click\')');
+    res.send(html);
+    res.end();
+})
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
